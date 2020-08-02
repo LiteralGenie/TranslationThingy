18006260c0e5db58052eef93be70525b740d2d9a
Literal Genie <frothsavd@gmail.com>
2020-08-01 21:04:27 -0400

    merged constant paths: ROOT_DIR, PROJECT_DIR

--- utils/globals.py
+++ utils/globals.py
@@ -1,14 +1,13 @@
-ROOT_DIR= "C:/Programming/"
-PROJECT_DIR= ROOT_DIR + "KnightRunReader/"
+ROOT_DIR= "C:/Users/Anne/PycharmProjects/KRR/KRR/"
 
-CONFIG_DIR= PROJECT_DIR + "config/"
+CONFIG_DIR= ROOT_DIR + "config/"
 
-DATA_DIR= PROJECT_DIR + "data/"
+DATA_DIR= ROOT_DIR + "data/"
 ENDIC_DIR= DATA_DIR + "endic/"
 OCR_DIR= DATA_DIR + "ocr/"
 PAPAGO_CACHE= DATA_DIR + "papago.json"
 
-GOOGLE_SERVICE_ACCOUNT= "981443074237-compute@developer.gserviceaccount.com"
+# GOOGLE_SERVICE_ACCOUNT= "981443074237-compute@developer.gserviceaccount.com"
 
-KIVY_UTILS_DIR= PROJECT_DIR + "kivy_utils/"
+KIVY_UTILS_DIR= ROOT_DIR + "kivy_utils/"
 KIVY_CLASS_DIR= KIVY_UTILS_DIR + "classes/"
