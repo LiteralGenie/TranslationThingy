e55bf9501ffe56c2287bbd14865d3dad4964c700
Genie <frothsavd@gmail.com>
2020-08-01 23:11:41 -0400

    VocabPanel almost done

--- kivy_utils/classes/vocab_tabs.py
+++ kivy_utils/classes/vocab_tabs.py
@@ -1,5 +1,5 @@
 from kivy.config import Config
-from kivy.properties import ObjectProperty, DictProperty
+from kivy.properties import ObjectProperty, DictProperty, StringProperty
 
 Config.set('kivy', 'log_level', 'debug')
 
@@ -16,14 +16,47 @@ from kivy.uix.tabbedpanel import TabbedPanelItem
 import utils
 Builder.load_file(utils.KIVY_CLASS_DIR + "vocab_tabs_layout.kv")
 
-class TabV(TabbedPanelItem):
-	layout= ObjectProperty(None)
+class WordCard(GridLayout):
+	name= StringProperty()
 
-	def build(self, name):
-		self.text= name
-		self.layout= self.ids.layout
+	def build(self, data):
+		self.ids.name.text= data['name']
+
 		return self
 
+class TabV(TabbedPanelItem):
+	layout= ObjectProperty(None)
+	words= DictProperty()
+
+	def build(self, name, words=None):
+		self.text= name
+		self.layout= self.ids.layout
+
+		if words: self.add_words(words)
+		return self
+
+	def add_words(self, words):
+		self.layout.cols= 10
+
+		for x in words:
+			self.words[x]= words[x]
+
+		self.clear_widgets()
+		for x in sorted(self.words.keys(), key=lambda y: words[y]['name']):
+			self.layout.add_widget(WordCard().build(words[x]))
+
+	def remove_words(self, words):
+		tmp= []
+		for x in words:
+			for y in self.words:
+				if x['name'] == y['name']:
+					tmp.append(y)
+
+		for y in tmp: del self.words[y]
+
+	def clear_words(self):
+		self.words= {}
+		self.clear_widgets()
 
 class VocabPanel(TabbedPanel):
 	tabs= DictProperty()
@@ -54,7 +87,17 @@ if __name__ == "__main__":
 	class TestApp(App):
 		def build(self):
 			self.root= BoxLayout()
-			self.root.add_widget(VocabPanel().build())
+
+			vocab= VocabPanel().build()
+			vocab.pinTab.add_words({
+				"1": { "name": "aaa" },
+				"2": { "name": "ccc" },
+				"3": { "name": "bbb" },
+				"4": { "name": "eee" },
+				"5": { "name": "ddd" },
+			})
+
+			self.root.add_widget(vocab)
 			return self.root
 
 	a= TestApp()
