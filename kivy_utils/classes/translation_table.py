e0a4f6d0ffea794050de5e8c6e86ee4a1539e957
Genie <frothsavd@gmail.com>
2020-08-03 07:05:34 -0400

    Added auto-scrolling to certain bubbles when a row-label is double clicked.

--- kivy_utils/classes/translation_table.py
+++ kivy_utils/classes/translation_table.py
@@ -9,7 +9,10 @@ from kivy.properties import ObjectProperty, NumericProperty, ListProperty
 Builder.load_file(utils.KIVY_CLASS_DIR + "translation_table_layout.kv")
 
 class Row(BoxLayout):
-	def build(self, row_index, kor="", eng="", on_focus=None):
+	on_focus= [] # @TODO: rename on_focus to on_click
+	on_double_click= []
+
+	def build(self, row_index, kor="", eng=""):
 		self.row_index= int(row_index) # Row Index @TODO: prevent duplicate indices?
 
 		self.num= RowLabel(text=str(self.row_index)) # Label for row number (1-indexed, ignoring header row)
@@ -20,10 +23,9 @@ class Row(BoxLayout):
 		self.add_widget(self.kor)
 		self.add_widget(self.eng)
 
-		self.on_focus= on_focus
-
 		return self
 
+	# click events
 	def on_touch_down(self, touch):
 		if self.collide_point(*touch.pos) and self.on_focus:
 			if touch.button == "left":
@@ -36,32 +38,46 @@ class TranslationTable(ScrollView):
 	row_height= NumericProperty(60)
 
 	# Add to or regenerate the table entries
-	def build(self, row_height=None, clear=True):
+	def build(self, row_height=None, clear=True, on_focus=None):
 		if clear:
 			if self.rows: self.layout.clear_widgets(self.rows) # Falsy values will clear ALL children
 			self.rows= []
+			self.pages_bubbles_rows= []
+		if on_focus: Row.on_focus= on_focus
 
 		self.layout= self.ids.layout # BoxLayout
 		if row_height: self.row_height= row_height
 
 		for x in self.rows: self.layout.add_widget(x)
-
 		return self
 
-	def append_row(self, kor="", eng="", on_focus=None):
-		r= Row().build(row_index=len(self.rows), kor=kor, eng=eng, on_focus=on_focus)
+	def append_row(self, kor="", eng=""):
+		r= Row().build(row_index=len(self.rows)+1, kor=kor, eng=eng)
 		self.rows.append(r)
 		self.layout.add_widget(r)
 
 		return r
 
-	def insert_row(self, index, kor="", eng="", on_focus=None):
-		r= Row().build(row_index=index, kor=kor, eng=eng, on_focus=on_focus)
+	def insert_row(self, index, kor="", eng=""):
+		r= Row().build(row_index=index, kor=kor, eng=eng)
 		self.rows.insert(index, r)
 		self.build(clear=False)
 
 		return r
 
+	def populate_from_pages(self, pages, on_focus=None):
+		self.build(on_focus=on_focus)
+
+		for pg in pages:
+			for bubb in pg.bubbles:
+				r= self.append_row(kor=bubb.raw_text)
+				self.pages_bubbles_rows.append({"bubble": bubb, "row": r, "page": pg})
+
+	@property
+	def on_focus(self): return Row.on_focus
+
+	@property
+	def on_double_click(self): return Row.on_double_click
 
 class KorInput(TextInput): pass
 class EngInput(TextInput): pass
