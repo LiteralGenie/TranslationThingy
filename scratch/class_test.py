63bed671950cb3872a4159ea507724ac17e06f84
Literal Genie <frothsavd@gmail.com>
2020-08-05 13:59:47 -0400

    cleaning

--- scratch/class_test.py
+++ scratch/class_test.py
@@ -6,12 +6,14 @@ from kivy.uix.textinput import TextInput
 
 Config.set('kivy', 'log_level', 'debug')
 
+from kivy.graphics import Color, Line
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.recycleview import RecycleView
 
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelItem
@@ -22,15 +24,16 @@ kv = """
 <Test>: # BoxLayout
     layout: layout
     
-    ScrollView:
-        size_hint: (1,1)
-        smooth_scroll_end: 500
-        
-        
+    RecycleView:
+        height: 1000
         BoxLayout:
             id: layout
-            orientation: "horizontal"
-            size_hint: (None,None)
+            orientation: "vertical"
+            size_hint_y: None
+            height: 1000
+            
+<Label>:
+    height: 100            
 """
 Builder.load_string(kv)
 
@@ -38,29 +41,20 @@ class Test(BoxLayout):
     layout= ObjectProperty()
 
     def populate(self, num=2):
-        im_path= "C:/Users/Anne/PycharmProjects/KRR/gralb2.png"
+        for i in range(1,num+1):
+            # label= Label(text=f"Label {i}")
+            label= AsyncImage(source=r"C:\Users\Anne\PycharmProjects\KRR/gralb.png")
 
-        for i in range(num):
-            im_dims= tuple(reversed(cv2.imread(im_path).shape[:2]))
-            im= AsyncImage(
-                source=im_path,
-                size_hint=(None, None),
-                size=im_dims
-            )
-            self.layout.add_widget(im)
-
-            if self.layout.orientation == "vertical":
-                self.layout.height+= im_dims[1]
-                self.layout.width= im_dims[0]
-            else:
-                self.layout.width+= im_dims[0]
-                self.layout.height= im_dims[1]
+            with label.canvas:
+                Color(1,0,0, 1)
+                Line(points=[100*i,200*i, 300*i,400*i], width=5)
+            self.layout.add_widget(label)
 
         return self
 
 class TestApp(App):
     def build(self):
-        self.root= Test().populate(num=500)
+        self.root= Test().populate(num=5)
         return self.root
 
 if __name__ == '__main__':
