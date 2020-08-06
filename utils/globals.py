071d9d9f90a2fa32265dfbde5b31f9da4bad7afc
Genie <frothsavd@gmail.com>
2020-08-05 21:54:52 -0400

    more gitignore

--- utils/globals.py
+++ utils/globals.py
@@ -1,17 +1,22 @@
 import pathlib
+from utils.misc_utils import loadJson
 
 ROOT_DIR= str(pathlib.Path(__file__).parent.parent) + "/"
 
-CONFIG_DIR= ROOT_DIR + "config/"
 
 DATA_DIR= ROOT_DIR + "data/"
+KIVY_UTILS_DIR= ROOT_DIR + "kivy_utils/"
+CONFIG_DIR= ROOT_DIR + "config/"
+
+
+FONT_DIR= DATA_DIR + "/fonts"
 ENDIC_DIR= DATA_DIR + "endic/"
 OCR_DIR= DATA_DIR + "ocr/"
 PAPAGO_CACHE= DATA_DIR + "papago.json"
 
-# GOOGLE_SERVICE_ACCOUNT= "981443074237-compute@developer.gserviceaccount.com"
 
-KIVY_UTILS_DIR= ROOT_DIR + "kivy_utils/"
+CONFIG= loadJson(CONFIG_DIR + "config.json")
+FONT_PATH_1= DATA_DIR + CONFIG['font_1']
+
+
 KIVY_CLASS_DIR= KIVY_UTILS_DIR + "classes/"
-
-FONT_PATH= DATA_DIR + "fonts/NotoSansKR-Regular.otf"
